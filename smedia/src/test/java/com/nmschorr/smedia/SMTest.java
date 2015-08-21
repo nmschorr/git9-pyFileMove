f8da89aecf669d4fe68b81d1435be47dc479865e
nmschorr <nancyschorr@yahoo.com>
2015-08-20 21:07:26 -0700

    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

---
+++ smedia/src/test/java/com/nmschorr/smedia/SMTest.java
@@ -0,0 +1,250 @@
+package com.nmschorr.smedia;
+
+/***
+ * SMTest.java by Nancy M. Schorr, 2015
+ * 
+ * This test suite executes four tests against schorrmedia.com
+ * 
+ * Note: 
+ *
+ * The tests are run with Junit, so each test stands on its own and 
+ * can be run independently of the others.  Each test launches a fresh 
+ * instance of Firefox with Selenium WebDriver.  The tests use Maven and a 
+ * Maven pom.xml file to specify dependencies.  The entire suite was written 
+ * in and runs with Eclipse on a Windows 7 PC. 
+ *
+ * Please see README.md for more information.
+ * 
+ */
+
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.runners.MethodSorters;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.support.ui.Select;
+
+import static java.lang.System.out;
+import com.nmschorr.smedia.SMTestUtil;
+
+// note: you need to have an account and password at SM.com
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class SMTest extends SMTestUtil {
+	SMTestUtil zutil = new SMTestUtil();
+
+	@Before  //run only once before all tests
+	public void setUp() throws Exception {		
+		createProperties();
+		gLogger = zutil.createLogger();
+		zDriver = createDriver();
+	}
+
+	@Test
+	public void Test1LoginSM() throws Exception {
+		// eClass is an empty class there just for returning the local method name
+		// test logging in only	    
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod());
+		loginSM(SMTestMainUrl);
+		logoutSM();
+	}
+
+	@Test
+	public void Test2SMAddPortfolio() throws Exception {
+		// test adding a portfolio
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod());
+
+		printMe("loginSM(SMMainUrl)");
+		loginSM(SMTestMainUrl);
+		  
+		deletePortfolio();
+		printMe("SMAddPortfolio()");
+		SMAddPortfolio();	    
+		printMe("logoutSM()");
+		logoutSM();
+	}
+
+	//@Test
+	public void Test3SMModifyPortfolio() throws Exception {
+		// test modifying a portfolio
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod());
+
+		printMe("loginSM(SMMainUrl)");
+		loginSM(SMTestMainUrl);
+		printMe("modifyPort()");
+		modifyPort();
+		printMe("logoutSM()");
+		logoutSM();
+	} 
+
+	//@Test
+	public void Test4SMDeletePortfolio() throws Exception {
+		// test deleting a portfolio
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod());
+
+		printMe("loginSM(SMMainUrl)");
+		loginSM(SMTestMainUrl);
+		printMe("deletePortfolio()");
+		deletePortfolio();
+		printMe("logoutSM()");
+		logoutSM();
+	} 
+	// end  
+
+	static void removeOverlay() {     // get rid of intrusive overlay alert ad
+		printMe("About to dismiss overlay.");
+	    boolean yesNo  = isElementPresent(By.cssSelector("img"));  // get rid of intrusive overlay alert ad
+		out.println("Value of finding overlay: " + yesNo);
+    if (yesNo == true)  zDriver.findElement(By.cssSelector("img")).click();
+	}
+    
+	// beginning of methods to support tests
+	static boolean isElementPresent(By by) {
+		    try {
+		      zDriver.findElement(by);
+		      return true;
+		    } catch (NoSuchElementException e) {
+		      return false;
+		    }
+		  }
+
+
+	public static void loginSM(String localUrl) {  
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod());
+		
+		String errorString = "ERROR - no value present";
+		String signinval =  theProperties.getProperty("signinval", errorString);
+		String usernameval =  theProperties.getProperty("usernameval", errorString);
+		String pwordval =  theProperties.getProperty("pwordval", errorString);
+
+		zDriver.get(localUrl);  // main SM page
+		
+		//removeOverlay();
+		printMe("setWindowSize()");
+		setWindowSize();
+
+		printMe("zDriver.findElement(By.linkText(Sign In)).click()");				
+		zDriver.findElement(By.linkText("Sign In")).click();		
+		zDriver.findElement(By.id("username")).clear();
+
+		//zDriver.findElement(By.id("username")).sendKeys(usernameval);
+		zfindAndType("username",usernameval);
+
+		zDriver.findElement(By.id("password")).clear();
+		printMe("zDriver.findElement(By.id(password)).sendKeys(pwordval)");
+		zDriver.findElement(By.id("password")).sendKeys(pwordval);	 
+		zDriver.findElement(By.xpath(signinval)).click();
+		assertEquals("Testing Signin",theTitle, zDriver.getTitle());					
+	}
+
+	public static void SMAddPortfolio() throws Exception {
+		// method to add portfolio, must be logged in first
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod()); 	
+		printMe("zDriver.get(portfolioUrl)");
+		zDriver.get(portfolioUrl);
+		assertEquals("Testing Portfolio Url Title", portfolioTitle, zDriver.getTitle());    
+		printMe("zDriver.findElement(By.linkText(Create a New Portfolio)).click()");
+		zDriver.findElement(By.linkText("Create a New Portfolio")).click(); 
+
+		String expectedTitle = "Stock Portfolio Management - SM Investment Research";
+		assertEquals(expectedTitle, zDriver.getTitle());
+
+		// add new portfolio entries
+		printMe("zDriver.findElement(By.name(port_name)).clear()");
+		zDriver.findElement(By.name("port_name")).clear();
+		zDriver.findElement(By.name("port_name")).sendKeys(PF_NAME);
+		zDriver.findElement(By.name("port_tck_name")).clear();
+		printMe("zDriver.findElement(By.id(port_tck_name)).sendKeys(aapl, msft, t, csco, irbt)");
+		zDriver.findElement(By.id("port_tck_name")).sendKeys("aapl, msft, t, csco, irbt");
+		out.println("running this next: " + "zDriver.findElement(By.id(submit)).click();");
+		printMe("zDriver.findElement(By.id(submit)).click()");
+		zDriver.findElement(By.id("submit")).click();
+		zDriver.findElement(By.xpath("//input[@value='Modify Portfolio Buys']")).click();  
+	}
+
+	public static void modifyPort() throws Exception {
+		// modify portfolio - add shares
+		class eClass {};	    
+		printMethodName(eClass.class.getEnclosingMethod());	
+
+		printMe("zDriver.get(portfolioUrl)");
+		zDriver.get(portfolioUrl);  // main SM page
+		String expectedTitle = "Stock Portfolio Management - SM Investment Research";
+		out.println("Expected title is: " + zDriver.getTitle());
+		assertEquals("Testing Portfolio Url Title", expectedTitle, zDriver.getTitle());    
+
+		printMe("zDriver.findElement(By.linkText(Modify Previous Buys)).click()");
+		zDriver.findElement(By.linkText("Modify Previous Buys")).click();
+		zDriver.findElement(By.name("position[0][shares]")).clear();
+
+		zfindAndType("position[0][shares]","22");
+		//zDriver.findElement(By.name("position[0][shares]")).sendKeys("22");
+
+		zDriver.findElement(By.name("position[1][shares]")).clear();
+		zDriver.findElement(By.name("position[1][shares]")).sendKeys("33");
+		zDriver.findElement(By.name("position[2][shares]")).clear();
+		zDriver.findElement(By.name("position[2][shares]")).sendKeys("44");
+		zDriver.findElement(By.name("position[3][shares]")).clear();
+
+		zfindAndType("position[3][shares]","55");
+		//zDriver.findElement(By.name("position[3][shares]")).sendKeys("55");
+
+		zDriver.findElement(By.name("position[4][shares]")).clear();
+		zDriver.findElement(By.name("position[4][shares]")).sendKeys("66");
+		zDriver.findElement(By.id("modify_btn")).click();
+	}
+
+
+	public void deletePortfolio ()  {
+		// need to add check to sure the portfolio is there to delete
+		out.println("Deleting test portfolio only if it exists already.");
+		zDriver.get(portfolioUrl);
+		zDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //for the entire test run
+		new Select(zDriver.findElement(By.id("port_id"))).selectByVisibleText(PF_NAME);
+		boolean keepgoing = chklink();
+		//removeOverlay();
+		if (keepgoing == true) {
+			mySleep(1);
+			zDriver.findElement(By.linkText("Delete this Portfolio")).click();
+			zDriver.findElement(By.id("chk")).click();
+			zDriver.findElement(By.name("btn_del")).click();
+			mySleep(1);
+			System.out.println("title is: " + zDriver.getTitle());
+			assertThat(zDriver.getTitle(), containsString("Stock Portfolio Management"));
+			// page : <title>Stock Portfolio Management - SM Investment Research</title>
+		}
+		zDriver.manage().timeouts().implicitlyWait(WAIT_TIME, TimeUnit.SECONDS); //for the entire test run
+	}
+
+
+	@After
+	public void tearDown() throws Exception {
+		gLogger.exit(false);
+		AlertThread thread3=new AlertThread();  
+		thread3.start();  
+		gLogger.info("Quitting Webdriver and shutting down");
+		zDriver.quit();
+		mySleep(1);
+		gLogger.info("All done with tests and exiting. Goodbye.");
+		//  String verificationErrorString = verificationErrors.toString();
+		//	  if (!"".equals(verificationErrorString)) {
+		//		  fail(verificationErrorString);
+	}
+
+}
