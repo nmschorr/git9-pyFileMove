1b55e7ac34a539f817e5fafb405133956945fc96
nmschorr <nancyschorr@yahoo.com>
2015-12-08 23:04:26 -0800

    Rewrote files.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

---
+++ moveFiles/moveFile/movingFiles/moveit.py
@@ -0,0 +1,53 @@
+'''
+Created on Nov 22, 2015
+@author: Nancy Schorr
+'''
+##  You need to create a file called "fileslist.py" in the the same directory as this file
+##  inside fileslist.py you need a line that contains the file types to move 
+##  Put one line in the file the defines the list dirTypes like this:
+##  dirTypes = ["pdf", "cat", "car", "aaa", "ring", "avery", "avalon",  "jpg", "doc" ]
+
+import re
+import os
+import shutil
+
+from fileslist import dirTypes
+
+if __name__ == '__main__':   pass
+
+
+workDir= 'C:\\Users\\user\\Desktop\\MYDOCS'
+outDir='C:\\Users\\user\\Desktop\\moved'
+quotes = '"'
+ 
+def mywalk(inputDir):
+    for rootDir, subDirs, eachFile in os.walk(inputDir):
+        fullFilePathName=[rootDir,subDirs,eachFile]     #--------ss note1
+        print "-------------  working on " + str(fullFilePathName)
+       
+        for efile in eachFile:
+            print "!!-----------------------------starting over --newval equals: " +  efile
+            for fName in dirTypes:
+                print "fName is: " + fName
+             
+                if re.search(fName, efile, re.IGNORECASE ):
+                    print "FOUND MATCH. efile is: " + str(efile)
+                   
+                    oldpath =  os.path.join(rootDir, efile)
+                    newdir =  "c:/Users/user/Desktop/moved/" + fName
+                    print "rootDir is: " + rootDir
+                   
+                    print "oldpath is: " + oldpath
+                    print "newdir is: " + newdir
+                    print(  os.path.isfile(oldpath) )                 
+                    print(  os.path.isdir(newdir) )                 
+                    shutil.move(oldpath, newdir)
+                    print "-------done with a loop"
+                    break
+                
+                        
+mywalk(workDir)
+
+print "end of program"
+
+
