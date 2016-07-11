34c81442c04cf4f5bd55a8d9227e8ab2ad83daac
nmschorr <nancyschorr@yahoo.com>
2016-07-11 15:49:26 -0700

    Finished first version
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- TestRestwJson/src/TestRestsJson.java
+++ TestRestwJson/src/TestRestsJson.java
@@ -1,93 +1,50 @@
-//import java.io.File;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import org.apache.commons.io.FileUtils;
-//import java.io.FileNotFoundException;
-//import java.util.Iterator;
-//import org.json.simple.JSONArray;
-//import org.json.simple.parser.ParseException;
-//import org.json.*;
 
 
-import java.io.FileReader;
-import java.net.*;
-import java.nio.charset.*;
-import java.net.URL;
-import org.apache.commons.io.IOUtils;
+import java.io.*;
 import org.json.*;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
+import java.util.Scanner;
+//import java.net.*;
+//import java.nio.charset.*;
+//import java.net.URL;
+//import org.apache.commons.io.IOUtils;
+
+
 
 public class TestRestsJson {
-  		 
+	public static String myFilenm = "C:\\jasondata2.json";
+	public static String myString = null;
+	public static String myDelim = "\\r";
+	public static String myDelim2 = "\\Z";
+	
+
 	public static void main(String[] args) {
-		//makelist();
-		readja();
+		readJson();
 		System.out.println("Finished");
 	}
-	
-	static void readja() {
+
+	static void readJson() {
 		try {
-			JSONParser parser = new JSONParser();
-			String myFilenm = "C:\\jasondata2.json";
-			Object fileObject = parser.parse(new FileReader(myFilenm));
-			String[] mylist = {"id", "name"};
+			File myFile = new File(myFilenm);
+			Scanner myScanner = new Scanner(myFile);
 			
+			myString = myScanner.useDelimiter(myDelim2).next();
+			System.out.println(myString);
 			
-			JSONObject jsonObject = new JSONObject(fileObject,mylist);
-			System.out.println("Here's the json object:");
-			System.out.println(jsonObject.toString());
-			System.out.println("Here's the json object again:");
-			System.out.println(jsonObject.toJSONString());
-			String newString = (String) jsonObject.get("username");
-			System.out.println("username: " + newString);
-			 
-		
+			JSONArray myArry = new JSONArray(myString); 
+			myScanner.close();
+
+			for(int i=0; i<myArry.length(); i++){
+				JSONObject jsonObj  = myArry.getJSONObject(i);
+				System.out.println("Value of id:" + jsonObj.get("id"));
+				System.out.println("Done with record");
+			}			
+			
 		} catch (Exception e) { 
-			System.out.println("json object didn't work!");
+			System.out.println("Exception caught - something didn't work!");
 			e.printStackTrace();	
 		}
+		finally {}
 	}
-
-	
-	static void makelist() {
-		try {
-			
-		    InetAddress address = InetAddress.getByName("192.168.1.254");
-		    boolean reachable = address.isReachable(100000);
-		    System.out.println("Is host1 reachable? " + reachable);		
-		    
-		    InetAddress address2 = InetAddress.getByName("192.168.1.64");
-		    boolean reachable2 = address2.isReachable(100000);
-		    System.out.println("Is host google reachable? " + reachable2);		
-			Document doc = Jsoup.connect( "http://www.google.com:80" ).userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0").referrer("http://www.google.com").timeout(1000*5).ignoreHttpErrors(true).get();
-			System.out.println( doc.toString() );		
-			System.out.println("google worked!");
-		} catch (Exception e) { 
-			System.out.println("google didn't work!");
-			e.printStackTrace();	
-		}
-				
-				
-				
-				
-		try {
-			URL myUrl = new URL("http://jsonplaceholder.typicode.com/users");
-			String myUrlStr = IOUtils.toString(myUrl, Charset.forName("UTF-8"));	
-			//JSONObject obj = (JSONObject) new JSONTokener(myUrlStr);
-            JSONObject jsonObject = (JSONObject) JSONValue.parseWithException(myUrlStr);		
-            System.out.println(jsonObject.get("id"));
-
-		} catch (Exception e) { 
-			e.printStackTrace();	
-		}	// catch
-
-		 
-	}
-
-
 }
+
+
