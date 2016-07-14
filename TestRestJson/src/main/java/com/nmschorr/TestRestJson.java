3ae68cc1144645439c5d659d9a5eee392a6ddf6c
nmschorr <nancyschorr@yahoo.com>
2016-07-14 15:57:48 -0700

    Final fixes
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
+++ TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
@@ -1,5 +1,5 @@
 /* TestRestsJson by Nancy Schorr, 2016
- Demonstrates use of apis to read text file and store it as a Json object for further manipulation
+ Demonstrates use of apis to read text file and store it as a JSON object for further manipulation
  */
 
 package com.nmschorr;
@@ -15,60 +15,55 @@ public class TestRestJson {
 	public static String myString = null;
 	public static String myDelim = "\\r";
 	public static String myDelim2 = "\\Z";
-	public static Map myMap;
+	public static Map<Integer, String> f = new HashMap<>();
+ 
 
 	public static void main(String[] args) {
-		readJson();
+		try {
+			readJson();
+		} 
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 		System.out.println("Program Finished");
 	}
 
-	static void readJson() {
+	static void readJson() throws Exception {
 		myString = myScanner();
+		List<Object> mlist = new ArrayList<Object>();
 
-		try {
-			JSONArray myJsonArry = new JSONArray(myString); 
-			JSONObject myJsonObj = myJsonArry.optJSONObject(0);
+		JSONArray myJsonArry = new JSONArray(myString); 
+		mlist = toList(myJsonArry);
+		Object tempObj = mlist.get(0);
+		 
+		
+		HashMap<String, String> map = (HashMap<String, String>) tempObj;
+		
+		
+		JSONObject myJsonObj = myJsonArry.optJSONObject(0);
+		Iterator<Object> myJsonItr = myJsonArry.iterator();
 
-			Iterator myJsonItr = myJsonArry.iterator();
-
-			while(myJsonItr.hasNext()) {
-				Object myObj = myJsonItr.next();
-				// myMap = (Map)myJsonArry.getJSONArray(0);
-				System.out.println("myJsonArry element obj" + myObj + " ");
-			}
-
+		while(myJsonItr.hasNext()) {
+			Object myObj = myJsonItr.next();
+			System.out.println("myJsonArry element obj" + myObj + " ");
 			System.out.println("--------------------" + myJsonObj.toString());
 			ArrayList<String> a = new ArrayList<String>();
-
-
 			for(int i=0; i<myJsonArry.length(); i++){
 				JSONObject jsonObj  = myJsonArry.getJSONObject(i);
 			} //for
-		} //try
 
-		catch (Exception e) { 
-			System.out.println("Exception caught - something didn't work!");
-			e.printStackTrace();	
-		}
+		}//while
 	}
 
 
-	public static String myScanner() {
+	public static String myScanner() throws FileNotFoundException {
 		final Scanner myScanner;
 		String locString = new String(" ");
 		File myFile = new File(myFilenm);
-		try {
-			myScanner = new Scanner(myFile);
-			locString = myScanner.useDelimiter(myDelim2).next();
-			myScanner.close();			
-			//System.out.println(myString);
-		}
-		catch (Exception e) { 
-			System.out.println("Exception caught - something didn't work!");
-			e.printStackTrace();	
-		}
+		myScanner = new Scanner(myFile);
+		locString = myScanner.useDelimiter(myDelim2).next();
+		myScanner.close();			
 		return locString;
-
 	}  //myScanner()
 
 
@@ -85,6 +80,7 @@ public class TestRestJson {
 		return retMap;
 	}
 
+	// from: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 	public static Map<String, Object> toMap(JSONObject object) throws JSONException {
 		Map<String, Object> map = new HashMap<String, Object>();
 
@@ -105,6 +101,7 @@ public class TestRestJson {
 		return map;
 	}
 
+	// from: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 	public static List<Object> toList(JSONArray array) throws JSONException {
 		List<Object> list = new ArrayList<Object>();
 		for(int i = 0; i < array.length(); i++) {
