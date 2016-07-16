307870dd0da3322bdb19892c2bd4589e4bc23c21
nmschorr <nancyschorr@yahoo.com>
2016-07-15 23:46:49 -0700

    Adding in Jersey and Jackson
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
+++ TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
@@ -11,6 +11,13 @@ import java.util.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import javax.ws.rs.client.*;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.core.*;
 
 //import javax.ws.rs.Path;
 //import javax.ws.rs.Produces;
@@ -22,12 +29,19 @@ public class TestRestJson {
 	public static String myDelim = "\\r";
 	public static String myDelim2 = "\\Z";
 	public static Map<Integer, String> f = new HashMap<>();
+	public static String jUrl = "http://jsonplaceholder.typicode.com/albums";
  
 
 	public static void main(String[] args) {
 		try {
 			readJson();
-		} 
+			restJersey();
+			
+			ObjectMapper mapper = new ObjectMapper(); // just need one
+			
+			System.out.println("Here's new stuff");
+		    System.out.println(mapper.toString());
+		   	} 
 		catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -60,13 +74,22 @@ public class TestRestJson {
 
 		}//while
 	}
-
-	public static void restJersey () throws Exception {
+	 
+	public static void restJersey() throws Exception {
+		String jsonUrl = "http://jsonplaceholder.typicode.com";
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://foo").path("bar");
-		Invocation.Builder invocationBuilder = target.request(MediaType.TEXT_PLAIN_TYPE);
+	 	WebTarget webTarget = client.target(jsonUrl).path("albums");
+		 
+		Invocation.Builder invocationBuilder =  webTarget.request(MediaType.TEXT_PLAIN_TYPE);
+
+
+		
 		Response response = invocationBuilder.get();
-	}
+		System.out.println("yes");
+		Object myobj = response.getEntity();
+		System.out.println("Server response .... \n");
+		System.out.println(myobj.toString());
+ 	}
 	
 	
 	
@@ -132,6 +155,26 @@ public class TestRestJson {
 		return list;
 	}
 
+	  private static String readAll(Reader rd) throws IOException {
+		    StringBuilder sb = new StringBuilder();
+		    int cp;
+		    while ((cp = rd.read()) != -1) {
+		      sb.append((char) cp);
+		    }
+		    return sb.toString();
+		  }
+
+		  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+		    InputStream is = new URL(url).openStream();
+		    try {
+		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+		      String jsonText = readAll(rd);
+		      JSONObject json = new JSONObject(jsonText);
+		      return json;
+		    } finally {
+		      is.close();
+		    }
+		  }
 
 }	//class
 
