0a135cee9c672e6acc49fea196072a07b24cc1e9
nmschorr <nancyschorr@yahoo.com>
2016-07-16 00:40:38 -0700

    Inputstream added
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
+++ TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
@@ -1,27 +1,15 @@
 /* TestRestsJson by Nancy Schorr, 2016
  Demonstrates use of apis to read text file and store it as a JSON object for further manipulation
+ Also goes to url http://jsonplaceholder.typicode.com/albums and reads the data
  */
 
 package com.nmschorr;
 
 import java.io.*;
 import org.json.*;
-import java.util.Scanner;
 import java.util.*;
-import javax.ws.rs.*;
-import javax.ws.rs.core.*;
-import javax.ws.rs.client.*;
 import java.net.URL;
-import java.nio.charset.Charset;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.core.*;
-
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.GET;
+import org.apache.commons.io.*;
 
 public class TestRestJson {
 	public static String myFilenm = "C:\\jasondata2.json";
@@ -33,32 +21,41 @@ public class TestRestJson {
  
 
 	public static void main(String[] args) {
+		System.setProperty("http.agent", "Chrome");
+		InputStream mystream=null;
+		
 		try {
-			readJson();
-			restJersey();
-			
-			ObjectMapper mapper = new ObjectMapper(); // just need one
-			
+			myString = myScanner();
+			readJson(myString);
 			System.out.println("Here's new stuff");
-		    System.out.println(mapper.toString());
-		   	} 
-		catch(Exception e) {
+			 
+			mystream = new URL( jUrl ).openStream();
+
+		    System.out.println( IOUtils.toString( mystream ) );
+		    String myString2 = IOUtils.toString( mystream );
+		    
+		    //readJson(myString);
+			System.out.println("new string");
+			System.out.println(myString2);
+				
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
+    	finally {
+	      IOUtils.closeQuietly(mystream);
+	    }			 
 		System.out.println("Program Finished");
 	}
 
-	static void readJson() throws Exception {
-		myString = myScanner();
-		List<Object> mlist = new ArrayList<Object>();
+	static void readJson(String myStr) throws Exception {
+		List<Object> myArrayList = new ArrayList<Object>();
 
-		JSONArray myJsonArry = new JSONArray(myString); 
-		mlist = toList(myJsonArry);
-		Object tempObj = mlist.get(0);
+		JSONArray myJsonArry = new JSONArray(myStr); 
+		myArrayList = toList(myJsonArry);
+		Object arryListObject = myArrayList.get(0);
+		HashMap<String,String> arryListObject2 = (HashMap<String, String>) myArrayList.get(0); // shows casting of Object to HashMap
 		 
-		
-		HashMap<String, String> map = (HashMap<String, String>) tempObj;
-		
+		HashMap<String, String> map = (HashMap<String, String>) arryListObject;  // shows casting of Object to HashMap
 		
 		JSONObject myJsonObj = myJsonArry.optJSONObject(0);
 		Iterator<Object> myJsonItr = myJsonArry.iterator();
@@ -67,7 +64,7 @@ public class TestRestJson {
 			Object myObj = myJsonItr.next();
 			System.out.println("myJsonArry element obj" + myObj + " ");
 			System.out.println("--------------------" + myJsonObj.toString());
-			ArrayList<String> a = new ArrayList<String>();
+			//ArrayList<String> a = new ArrayList<String>();
 			for(int i=0; i<myJsonArry.length(); i++){
 				JSONObject jsonObj  = myJsonArry.getJSONObject(i);
 			} //for
@@ -75,23 +72,6 @@ public class TestRestJson {
 		}//while
 	}
 	 
-	public static void restJersey() throws Exception {
-		String jsonUrl = "http://jsonplaceholder.typicode.com";
-		Client client = ClientBuilder.newClient();
-	 	WebTarget webTarget = client.target(jsonUrl).path("albums");
-		 
-		Invocation.Builder invocationBuilder =  webTarget.request(MediaType.TEXT_PLAIN_TYPE);
-
-
-		
-		Response response = invocationBuilder.get();
-		System.out.println("yes");
-		Object myobj = response.getEntity();
-		System.out.println("Server response .... \n");
-		System.out.println(myobj.toString());
- 	}
-	
-	
 	
 	public static String myScanner() throws FileNotFoundException {
 		final Scanner myScanner;
@@ -102,20 +82,7 @@ public class TestRestJson {
 		myScanner.close();			
 		return locString;
 	}  //myScanner()
-
-
-
-
-// from: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 	
-	public static Map<String, Object> jsonToMap(JSONObject json) throws JSONException {
-		Map<String, Object> retMap = new HashMap<String, Object>();
-
-		if(json != JSONObject.NULL) {
-			retMap = toMap(json);
-		}
-		return retMap;
-	}
 
 	// from: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 	public static Map<String, Object> toMap(JSONObject object) throws JSONException {
@@ -146,7 +113,6 @@ public class TestRestJson {
 			if(value instanceof JSONArray) {
 				value = toList((JSONArray) value);
 			}
-
 			else if(value instanceof JSONObject) {
 				value = toMap((JSONObject) value);
 			}
@@ -154,27 +120,6 @@ public class TestRestJson {
 		}
 		return list;
 	}
-
-	  private static String readAll(Reader rd) throws IOException {
-		    StringBuilder sb = new StringBuilder();
-		    int cp;
-		    while ((cp = rd.read()) != -1) {
-		      sb.append((char) cp);
-		    }
-		    return sb.toString();
-		  }
-
-		  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-		    InputStream is = new URL(url).openStream();
-		    try {
-		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-		      String jsonText = readAll(rd);
-		      JSONObject json = new JSONObject(jsonText);
-		      return json;
-		    } finally {
-		      is.close();
-		    }
-		  }
 
 }	//class
 
