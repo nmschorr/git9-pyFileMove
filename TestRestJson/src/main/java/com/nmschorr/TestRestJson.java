25137289fe5b9e129ed0fb05b7dc2967311afb54
nmschorr <nancyschorr@yahoo.com>
2016-07-22 16:53:08 -0700

    Minor edits for blog article readability
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
+++ TestRestJson/src/main/java/com/nmschorr/TestRestJson.java
@@ -13,23 +13,22 @@ import org.json.*;
 
 public class TestRestJson {
 	public static String scanFileContentsString = null;
-	public static String myDelim = "\\r";
-	public static String myDelim2 = "\\Z";
-	public static String myUrlString = "http://jsonplaceholder.typicode.com/albums";
+	public static String myDelim = "\\Z";
 	public static String myJsonFileName = "C:\\jasondata2.json";
 	static Exception e;
 
 	public static void main(String[] args) throws Exception {
 		System.setProperty("http.agent", "Chrome");
 		System.out.println("Beginning...");
-		readUrlData(myUrlString);  // reads json data from a url http page
+		readUrlData();  // reads json data from a url http page
 		readFile(myJsonFileName);  // read json data from a file
 		System.out.println("\nProgram Finished");
 	}
 
-	static void readUrlData (String locUrlString) throws Exception {
-		System.out.println("\nRunning readUrlData");
+	static void readUrlData () throws Exception {
 		String builderLine;
+		System.out.println("\nRunning readUrlData");
+		String locUrlString = "http://jsonplaceholder.typicode.com/albums";
 		Reader bufReader = new BufferedReader(
 				new InputStreamReader(new URL(locUrlString).openStream()));	
 		StringBuilder myStrBuilder = new StringBuilder();
@@ -40,10 +39,8 @@ public class TestRestJson {
 		out.println("\nDone with readUrlData ");
 	}
 
-
 	static void readFile(String fname) {
-		System.out.println("Running readFile");
-		System.out.println("name of file " + fname);
+		System.out.println("Running readFile. " + " name of file " + fname);
 		StringBuilder strBuilder = new StringBuilder();
 		try {
 			scanFileContentsString = readScanFile(fname);  // read from file
@@ -51,25 +48,18 @@ public class TestRestJson {
 			readJson(scanFileContentsString); 
 			System.out.println("in readFile: working on this: " + strBuilder.toString());
 			//System.out.println( "" );  // placeholder for debugger
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			//IOUtils.closeQuietly(in);
+		} catch(Exception e) { e.printStackTrace();
 		}
 	}
 
+		// This method just plays around with converting JSON objects: use debugger to see them
+		// List<Object> myArrayList = new ArrayList<Object>();
+		// HashMap<String, String> map = new HashMap<String, String>();  // shows casting of Object to HashMap
+		// Map<String,String> mapsObj = new HashMap<String,String>();	
 	static void readJson(String longJsonString) throws Exception {
-		// this method just plays around with converting JSON objects: use debugger to see them
-		//List<Object> myArrayList = new ArrayList<Object>();
-		//HashMap<String, String> map = new HashMap<String, String>();  // shows casting of Object to HashMap
-		//Map<String,String> mapsObj = new HashMap<String,String>();	
-
 		JSONArray myJsonArry = new JSONArray(longJsonString); 
 		List<Object> myArrayList = new ArrayList<Object>(toList(myJsonArry));
 		Collection<Map<String,String>> mapsCol = new HashSet<Map<String,String>>();	
-		Object myTempObj;
-
 		for (int i=0; i < myArrayList.size(); i++) {
 			mapsCol.add((HashMap<String, String>)myArrayList.get(i));
 		}
@@ -82,9 +72,10 @@ public class TestRestJson {
 			System.out.println("myJsonArry element obj" + myObj + " ");
 			System.out.println("--------------------" + myJsonObj.toString());
 			for(int i=0; i<myJsonArry.length(); i++){
-				JSONObject jsonObj  = myJsonArry.getJSONObject(i);
+				JSONObject jsonObj  = myJsonArry.getJSONObject(i);  // just to observe in debugger
 			} //for
 		} //while
+		//IOUtils.closeQuietly(in);
 	}
 
 	public static String readScanFile(String filename) throws FileNotFoundException {
@@ -92,12 +83,12 @@ public class TestRestJson {
 		String locString = new String("");  // new empty string
 		File myFile = new File(filename);
 		myScanner = new Scanner(myFile);
-		locString = myScanner.useDelimiter(myDelim2).next();
+		locString = myScanner.useDelimiter(myDelim).next();
 		myScanner.close();			
 		return locString;
 	}  //myScanner()
 
-	// thanks to: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
+	// for this and toList below: thanks to: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 	public static Map<String, Object> toMap(JSONObject object) throws JSONException {
 		Map<String, Object> myMap = new HashMap<String, Object>();
 		Iterator<String> keysItr = object.keys();
@@ -115,7 +106,6 @@ public class TestRestJson {
 		return myMap;
 	}
 
-	// thanks to: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 	public static List<Object> toList(JSONArray myJArray) throws JSONException {
 		List<Object> myList = new ArrayList<Object>();
 		for(int i = 0; i < myJArray.length(); i++) {
